ae5694eb581979270d226c708b4f62eb5f8f39e5
Poli <diegoroca.ordes@gmail.com>
2024-03-12 22:03:10 +0100

    daynight toggle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,42 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public static GameManager instance;
 
+    public Material matDay;
+    public GameObject dayLight;
+    public Material matNight;
+    public GameObject nightLight;
+
+    public List<GameObject> lights = new List<GameObject>();
+
+    public bool isDay = true;
+
+    // add an editor script to change the material of the skybox
+    public void ChangeSkybox(bool isDay)
+    {
+        if (isDay)
+        {
+            RenderSettings.skybox = matDay;
+            nightLight.SetActive(false);
+            dayLight.SetActive(true);
+
+            for (int i = 0; i < lights.Count; i++)
+            {
+                ((GameObject)lights[i]).SetActive(false);
+            }
+        }
+        else
+        {
+            RenderSettings.skybox = matNight;
+            dayLight.SetActive(false);
+            nightLight.SetActive(true);
+
+            for (int i = 0; i < lights.Count; i++)
+            {
+                ((GameObject)lights[i]).SetActive(true);
+            }
+        }
+    }
+
     private void Awake()
     {
         if (instance == null)
@@ -19,5 +55,14 @@ public class GameManager : MonoBehaviour
     public GameObject getPlayer()
     {
         return player;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            isDay = !isDay;
+            ChangeSkybox(isDay);
+        }
     }
 }
